01be99ecb0d00736b827c61d0ab5831b09497560
José Gamaliel Rivera Ibarra <j0s3.r1v3r@gmail.com>
2022-11-24 23:54:36 -0700

    Funciones de validación de roles y middleware auth

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,3 +1,6 @@
+import { User } from "./user";
+import { UserRoles } from "./user-roles";
+
 export * from "./roles.decorator";
 export * from "./roles.guard";
 export * from "./user-roles";
@@ -8,3 +11,57 @@ export const ROLES_USER_KEY = "roles";
 export const USER_REQ_KEY = "user";
 export const DEFAULT_AUTHORIZATION_HEADER = "authorization";
 export const API_GATEWAY_AUTHORIZATION_HEADER = "x-apigateway-api-userinfo";
+
+const PLATFORM_USER_ROLES: UserRoles[] = [
+  UserRoles.SUPER_ADMIN,
+  UserRoles.PLATFORM_ADMIN,
+  UserRoles.CONFIGURATOR
+];
+
+const INSTITUTE_USER_ROLES: UserRoles[] = [
+  UserRoles.ASSISTANT,
+  UserRoles.INSTITUTE_ADMIN,
+  UserRoles.INSTITUTE_EMPLOYEE,
+  UserRoles.TEACHER
+];
+
+const INSTITUTE_CLIENT_USER_ROLES: UserRoles[] = [
+  UserRoles.AUTHORIZED_PERSON,
+  UserRoles.STUDENT,
+  UserRoles.TUTOR
+];
+
+export function isPlataformAdmin(user: User): boolean {
+  return (
+    user.roles.includes(UserRoles.SUPER_ADMIN) ||
+    user.roles.includes(UserRoles.PLATFORM_ADMIN)
+  );
+}
+
+export function isInstituteConfigurator(user: User): boolean {
+  return (
+    user.roles.includes(UserRoles.INSTITUTE_ADMIN) ||
+    user.roles.includes(UserRoles.CONFIGURATOR)
+  );
+}
+
+export function isInstituteUser(user: User): boolean {
+  return (
+    user.roles.includes(UserRoles.ASSISTANT) ||
+    user.roles.includes(UserRoles.INSTITUTE_ADMIN) ||
+    user.roles.includes(UserRoles.INSTITUTE_EMPLOYEE) ||
+    user.roles.includes(UserRoles.TEACHER)
+  );
+}
+
+export function isPlataformUserRole(role: UserRoles): boolean {
+  return PLATFORM_USER_ROLES.includes(role);
+}
+
+export function isInstituteUserRole(role: UserRoles): boolean {
+  return INSTITUTE_USER_ROLES.includes(role);
+}
+
+export function isInstituteClientUserRole(role: UserRoles): boolean {
+  return INSTITUTE_CLIENT_USER_ROLES.includes(role);
+}
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,2 +1,31 @@
-export * from "./auth.middleware";
-export * from "./firebase-auth.middleware";
+import { NestMiddleware } from '@nestjs/common';
+import { VerifyOptions, Algorithm } from 'jsonwebtoken';
+import { AuthMiddleware, PublicKeyInfo } from './auth.middleware';
+import { FirebaseAuthMiddleware } from './firebase-auth.middleware';
+
+export * from './auth.middleware';
+export * from './firebase-auth.middleware';
+
+export function getAuthMiddleware(
+  getPublicKeyInfo?: (
+    kid: string,
+  ) => Promise<PublicKeyInfo | null> | PublicKeyInfo | null,
+): NestMiddleware {
+  const secret = process.env.AUTH_MIDDLEWARE_SECRET || '';
+  const algorithm = process.env.AUTH_MIDDLEWARE_ALGORITHM || 'HS256';
+  const issuer =
+    process.env.AUTH_MIDDLEWARE_ISSUER || 'https://gamarivera.dev/auth';
+  const audience =
+    process.env.AUTH_MIDDLEWARE_AUDIENCE || 'https://studenti.app';
+  const ignoreExpiration = process.env.NODE_ENV?.toLowerCase() == 'test';
+  const verifyOptions: VerifyOptions = {
+    algorithms: [algorithm as Algorithm],
+    issuer,
+    audience,
+    ignoreExpiration,
+  };
+  if (process.env.USE_AUTH_MIDDLEWARE?.toLowerCase() === 'true') {
+    return new AuthMiddleware(secret, getPublicKeyInfo, verifyOptions);
+  }
+  return new FirebaseAuthMiddleware();
+}
